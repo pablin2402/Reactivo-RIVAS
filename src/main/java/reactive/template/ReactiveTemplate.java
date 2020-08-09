86a94474b4150c70a8d7d811d5f6056f6e272ee1
pablin2402 <pabloperedo04@gmail.com>
2020-08-08 22:00:55 -0400

    Implementation of a method fot calculate the cost depending the action, transition method T(s,a, s') and some corrections in general

--- src/main/java/reactive/template/ReactiveTemplate.java
+++ src/main/java/reactive/template/ReactiveTemplate.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import entity.ActionEntity;
 import entity.State;
+import entity.ActionEntity.ActionKind;
 import logist.agent.Agent;
 import logist.behavior.ReactiveBehavior;
 import logist.plan.Action;
@@ -49,7 +50,7 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		for (City possibleCurrentCities : cities) {
 			allStates.add(new State(possibleCurrentCities, null));
 			for (City possibleNeighbors : cities) {
-				if (possibleCurrentCities != possibleNeighbors) {
+				if (!possibleCurrentCities.equals(possibleNeighbors)) {
 					allStates.add(new State(possibleCurrentCities, possibleNeighbors));
 				}
 			}
@@ -67,12 +68,30 @@ public class ReactiveTemplate implements ReactiveBehavior {
 	}
 
 	// It calculates the cost between the currentcity to the destiny
-	private static double calculateCost(State state, TaskDistribution td, Agent agent) {
+	private static double calculateCost(State state, TaskDistribution td, Agent agent, ActionEntity actionEntity) {
+		// it could be global....
 		double costPerKm = agent.vehicles().get(0).costPerKm();
 
+		if (actionEntity.getAction() == ActionEntity.ActionKind.Move) {
+			return -1 * state.getCurrentCity().distanceTo(actionEntity.getDestination()) * costPerKm;
+		}
 		// reward if it exist a task
-		return (double) td.reward(state.getCurrentCity(), state.getNeighbors())
+		return td.reward(state.getCurrentCity(), state.getNeighbors())
 				- (state.getCurrentCity().distanceTo(state.getNeighbors()) * costPerKm);
+	}
+
+	private double transition(State currentCity, ActionEntity action, State moveTo, TaskDistribution td){
+
+			if (action.getAction() == ActionEntity.ActionKind.Collect && !moveTo.getCurrentCity().equals(currentCity.getNeighbors()))
+			{
+				return 0;
+			}	
+			// move action
+			else if(action.getAction()==ActionEntity.ActionKind.Move&&action.getDestination().equals(moveTo.getCurrentCity())
+			{
+				return 0;
+			}
+			return td.probability(moveTo.getCurrentCity(),moveTo.getNeighbors());
 	}
 
 	@Override
@@ -105,9 +124,14 @@ public class ReactiveTemplate implements ReactiveBehavior {
 			for (State currentState : allStates) {
 				for (ActionEntity currentAction : allActions) {
 					// pickup action
+					double sum = 0;
+
 					if (currentState.getCurrentCity() != null
 							&& currentAction.getAction() == ActionEntity.ActionKind.Collect) {
-						qMax = calculateCost(currentState, td, agent);
+						for (State toState : allStates) {
+							qMax = calculateCost(currentState, td, agent, currentAction);
+							sum += transition(currentState, currentAction, toState, td) * v.getOrDefault(toState, 0.0);
+						}
 					}
 					// moveaction
 					else {
@@ -121,30 +145,6 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		Double discount = agent.readProperty("discount-factor", Double.class, 0.5);
 		this.random = new Random();
 		this.pPickup = discount;
-	}
-
-	private void probabilityfromCity(Topology topology, TaskDistribution td) {
-		List<City> cities = topology.cities();
-
-		int numberOfCities = cities.size();
-		for (int i = 0; i < numberOfCities; i++) {
-			City cityFrom = cities.get(i);
-			double cityProb = 0.0;
-			for (int j = 0; j < numberOfCities; j++) {
-				City cityTo = cities.get(j);
-				System.out.println("Probability for task from " + cityFrom.name + " to" + cityTo.name + " = "
-						+ td.probability(cityFrom, cityTo));
-				System.out.println("Reward for task from " + cityFrom.name + " to" + cityTo.name + " = "
-						+ td.reward(cityFrom, cityTo));
-
-				cityProb += td.probability(cityFrom, cityTo);
-			}
-			System.out.println("----------------------------");
-			System.out.println("Total Prob for " + cityFrom.name + " = " + cityProb);
-			System.out
-					.println("Prob of NO TASK: calc=" + (1 - cityProb) + " obtained=" + td.probability(cityFrom, null));
-			System.out.println();
-		}
 	}
 
 	@Override
